346ef46680403e739d75fdcf6a538159db7ca7ad
Nimrod Gutman <nimrod.g@singular.net>
2023-01-18 20:23:14 +0200

    change package name

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 
 from setuptools import setup, find_packages  # noqa: H301
 
-NAME = "mailchimp-transactional-python"
+NAME = "singular-mailchimp-transactional-python"
 VERSION = "1.0.47"
 # To install the library, run the following
 #
